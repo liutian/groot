6446323a567d10607afb1780ef36d086e476081c
liuss <1191577401@qq.com>
2022-02-09 02:07:41 +0800

    修复eslint

--- examples/webpack-demo/src/typings.d.ts
+++ examples/webpack-demo/src/typings.d.ts
@@ -7,6 +7,7 @@ declare namespace NodeJS {
   }
 }
 
+
 interface Window {
   define: (
     moduleName: string,
@@ -14,4 +15,9 @@ interface Window {
     wrapper: (...defaultModule: any) => void
   ) => void;
   _moduleCallback: (module: any) => void;
+
+  _grootProjectInfo: any;
 }
+
+
+
