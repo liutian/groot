da1ff1b9a6aeefce32e64e4f97c1f0b8e54e410a
liuss <1191577401@qq.com>
2023-02-13 10:58:31 +0800

    chore: 调整逻辑增加可读性，添加数组fill方法，避免，set避免不用执行

--- packages/common/reactive.ts
+++ packages/common/reactive.ts
@@ -1,7 +1,7 @@
 import { isBaseType } from './util';
 
 
-const ArrayPatchMethods = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+const ArrayPatchMethods = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse', 'fill'];
 
 export function wrapperState(target: any, listener: Function) {
   const targetTypeStr = Object.prototype.toString.apply(target);
@@ -17,43 +17,53 @@ export function wrapperState(target: any, listener: Function) {
       // 基本数据类型直接放行
       if (isBaseType(value)) {
         return value;
-      } else if (typeStr !== '[object Function]' && typeStr !== '[object AsyncFunction]') {
+      } else if (typeStr === '[object Function]' || typeStr === '[object AsyncFunction]') {
+        if (Array.isArray(oTarget) && typeof sKey === 'string' && ArrayPatchMethods.includes(sKey)) {
+          return (...args: any[]) => {
+            let newArgs = args;
+            switch (sKey) {
+              case 'push':
+              case 'unshift':
+                newArgs = args.map(item => wrapperState(item, listener))
+                break
+              case 'splice':
+                const insertItems = args.slice(2).map(item => wrapperState(item, listener));
+                newArgs = [args[0], args[1], ...insertItems]
+                break
+            }
+
+            const result = Reflect.apply(value, oTarget, newArgs);
+            listener();
+            return result;
+          }
+        }
+
+        // 非特殊数组函数外直接this指向
+        return value.bind(receiver);
+
+      } else {
         // React.Element不做任何处理
         if (value.$$typeof) {
           return value;
         }
-        // 除函数之外引用数据类型需要递归包裹生成代理对象
+        // 除函数之外引用应用类型需要递归包裹生成代理对象
         return wrapperState(value, listener);
-      } else if (!Array.isArray(oTarget) || !(typeof sKey === 'string' && ArrayPatchMethods.includes(sKey))) {
-        return value.bind(receiver);
-      }
-
-      return (...args: any[]) => {
-        let newArgs = args;
-        switch (sKey) {
-          case 'push':
-          case 'unshift':
-            newArgs = args.map(item => wrapperState(item, listener))
-            break
-          case 'splice':
-            const insertItems = args.slice(2).map(item => wrapperState(item, listener));
-            newArgs = [args[0], args[1], ...insertItems]
-            break
-        }
-
-        const result = Reflect.apply(value, oTarget, newArgs);
-        listener();
-        return result;
       }
     },
     set(oTarget, sKey, vValue) {
+      const origin = Reflect.get(oTarget, sKey);
       const result = Reflect.set(oTarget, sKey, vValue);
-      listener();
+      if (origin !== Reflect.get(oTarget, sKey)) {
+        listener();
+      }
       return result;
     },
     deleteProperty(oTarget, sKey) {
+      const origin = Reflect.get(oTarget, sKey);
       const result = Reflect.deleteProperty(oTarget, sKey);
-      listener();
+      if (origin !== Reflect.get(oTarget, sKey)) {
+        listener();
+      }
       return result;
     },
   })
