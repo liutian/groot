017541d398d46fae376172e703393c66703ef5a4
liuss <1191577401@qq.com>
2023-02-22 17:40:39 +0800

    fix: 修复代码编辑器属性项路径问题

--- packages/extension/src/util/monaco-loader.ts
+++ packages/extension/src/util/monaco-loader.ts
@@ -2,20 +2,27 @@ import { getContext } from "context";
 
 let loaderPromise: Promise<any>;
 
-export const monacoLoader = (path = `${getContext().extPackageUrl}/monaco-editor/vs`) => {
+export const monacoLoader = (path?: string) => {
+
+  let vsPath = path;
+  if (!vsPath) {
+    const packageUrl = getContext().extPackageUrl
+    const url = new URL(packageUrl)
+    vsPath = `${packageUrl.replace(url.pathname, '')}/monaco-editor/vs`
+  }
 
   return loaderPromise || (loaderPromise = new Promise((resolve) => {
     const scriptEle = document.createElement('script');
 
     scriptEle.addEventListener('load', () => {
-      (window['require'] as any).config({ paths: { vs: path } });
+      (window['require'] as any).config({ paths: { vs: vsPath } });
 
       (window['require'] as any)(['vs/editor/editor.main'], () => {
         resolve(null);
       });
     });
 
-    scriptEle.src = `${path}/loader.js`;
+    scriptEle.src = `${vsPath}/loader.js`;
     document.head.appendChild(scriptEle);
   }))
 }
